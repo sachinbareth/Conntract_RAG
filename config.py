41737b068b78953746810898375b29fa61bcdede
Sachin Bareth <136951702+sachinbareth@users.noreply.github.com>
2025-12-01 12:37:39 +0530

    Change PostgreSQL password in config.py
    Updated POSTGRES_URL to use 'postgres' as the password.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 POSTGRES_URL = os.getenv(
     "POSTGRES_URL",
-    "postgresql://postgres:Sach1234@localhost:5432/contractdb"
+    "postgresql://postgres:postgres@localhost:5432/contractdb"   # db credientials
 )
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
@@ -10,3 +10,4 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 EMBED_MODEL = "intfloat/multilingual-e5-base"
 CHUNK_SIZE = 800
 CHUNK_OVERLAP = 100
+
